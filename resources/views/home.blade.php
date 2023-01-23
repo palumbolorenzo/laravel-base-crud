b53cd5ae00b127305807e69693dd44a10ccbf8cf
palumbolorenzo <palumbolorenzo97@gmail.com>
2023-01-23 17:01:51 +0100

    provvisorio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+    <h1>home</h1>
+    <a class="btn btn-dark" href="http://127.0.0.1:8000/comic">view comics</a>
+    </div>
+
+    <nav class="navbar bg-dark">
+  <div class="container">
+    <!-- <a class="navbar-brand" href="#">
+      <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+    </a> -->
+    <h1>home</h1>
+    <a class="btn btn-dark" href="http://127.0.0.1:8000/comic">view comics</a>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/comic', function () {
+    return view('comic');
 });
